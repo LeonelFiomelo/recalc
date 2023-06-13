6a285dccb184b1ad7639b919f0944a06ffdeef0a
Leonel Fiomelo <leonelfiomelo@hotmail.com>
2023-06-13 16:33:53 -0300

    Se arreglo el bug se suma el cual al realizar una operación de suma entre dos números mostraba undefined. Tambien se realizo el test correspondiente para verificar que si se realiza y visualiza la suma correctamente

--- test/ui.test.js
+++ test/ui.test.js
@@ -48,4 +48,37 @@ test.describe('test', () => {
     expect(historyEntry.secondArg).toEqual(9)
     expect(historyEntry.result).toEqual(70)
   });
+
+  test('Deberia poder realizar una suma', async ({ page }) => {
+    await page.goto('./');
+
+    await page.getByRole('button', { name: '4' }).click()
+    await page.getByRole('button', { name: '8' }).click()
+    await page.getByRole('button', { name: '+' }).click()
+    await page.getByRole('button', { name: '2' }).click()
+
+    const [response] = await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/api/v1/add/')),
+      page.getByRole('button', { name: '=' }).click()
+    ]);
+
+    const { result } = await response.json();
+    expect(result).toBe(50);
+
+    await expect(page.getByTestId('display')).toHaveValue(/50/)
+
+    const operation = await Operation.findOne({
+      where: {
+        name: "ADD"
+      }
+    });
+
+    const historyEntry = await History.findOne({
+      where: { OperationId: operation.id }
+    })
+
+    expect(historyEntry.firstArg).toEqual(48)
+    expect(historyEntry.secondArg).toEqual(2)
+    expect(historyEntry.result).toEqual(50)
+  });
 })
